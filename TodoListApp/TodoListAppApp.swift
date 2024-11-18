189df0081753473e5744754fe9bcffa90c40ee79
hyliard <luisgerardomartinezh@gmail.com>
2024-11-18 15:59:56 -0300

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Hyliard on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
