08892202b833a19b44702326bb7854320295d56b
Hyliard <Luisgerardomartinezh@gmail.com>
2026-08-09 02:06:27 -0300

    Modernize app navigation

--- Todo/ToDoListView.swift
+++ Todo/ToDoListView.swift
@@ -10,69 +10,67 @@ import SwiftUI
 struct ToDoListView: View {
     @State private var todos: [TodoItem] = []
     @State private var newTodoTitle: String = ""
-    
+
     var body: some View {
-        
-        NavigationView {
-            VStack {
-                // Campo de entrada para agregar nuevas tareas
-                HStack {
-                    TextField("Nueva tarea...", text: $newTodoTitle)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-                    
-                    Button(action: {
-                        addTodo()
-                    }) {
-                        Image(systemName: "plus.circle")
-                            .font(.largeTitle)
-                            .foregroundColor(Color("backgroundButton"))
-                            .padding(.trailing)
-                    }
-                    .disabled(newTodoTitle.isEmpty)
+
+        VStack {
+            // Campo de entrada para agregar nuevas tareas
+            HStack {
+                TextField("Nueva tarea...", text: $newTodoTitle)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button(action: {
+                    addTodo()
+                }) {
+                    Image(systemName: "plus.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(Color("backgroundButton"))
+                        .padding(.trailing)
                 }
-                
-                // Lista de tareas
-                List {
-                    ForEach(todos) { todo in
-                        HStack {
-                            Button(action: {
-                                toggleCompletion(for: todo)
-                            }) {
-                                Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
-                                    .foregroundColor(todo.isCompleted ? .green : .gray)
-                            }
-                            
-                            Text(todo.title)
-                                .strikethrough(todo.isCompleted, color: .black)
-                        }
-                        
-                    }
-                    .onDelete(perform: deleteTodo)
-                }
-                .listStyle(InsetGroupedListStyle())
-                
+                .disabled(newTodoTitle.isEmpty)
             }
-            .background(Color("backgroundApp"))
-            .navigationTitle("Lista de Tareas ")
-            
+
+            // Lista de tareas
+            List {
+                ForEach(todos) { todo in
+                    HStack {
+                        Button(action: {
+                            toggleCompletion(for: todo)
+                        }) {
+                            Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                                .foregroundColor(todo.isCompleted ? .green : .gray)
+                        }
+
+                        Text(todo.title)
+                            .strikethrough(todo.isCompleted, color: .black)
+                    }
+
+                }
+                .onDelete(perform: deleteTodo)
+            }
+            .listStyle(InsetGroupedListStyle())
+
         }
+        .background(Color("backgroundApp"))
+        .navigationTitle("Lista de Tareas")
+
     }
-    
+
     // Función para agregar una nueva tarea
     private func addTodo() {
         let newTodo = TodoItem(title: newTodoTitle)
         todos.append(newTodo)
         newTodoTitle = ""
     }
-    
+
     // Función para marcar una tarea como completada
     private func toggleCompletion(for todo: TodoItem) {
         if let index = todos.firstIndex(where: { $0.id == todo.id }) {
             todos[index].isCompleted.toggle()
         }
     }
-    
+
     // Función para eliminar una tarea
     private func deleteTodo(at offsets: IndexSet) {
         todos.remove(atOffsets: offsets)
